a05658acd2c061a413d5342a9a7156fd81987c70
laureline <laureline.henaff63@gmail.com>
2026-01-08 10:33:33 +0100

    add files for allure reports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: ["eslint:recommended"],
-  rules: {
-    "no-unused-vars": "warn",
-    "prefer-const": "error",
-    eqeqeq: "error",
-    semi: ["error", "always"],
-  },
+    env: {
+        es2021: true,
+        node: true,
+        jest: true,
+    },
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module", // Permet d'utiliser 'import/export'
+    },
+    extends: ["eslint:recommended"],
+    rules: {
+        "no-unused-vars": "warn",
+        "prefer-const": "error",
+        eqeqeq: "error",
+        semi: ["error", "always"],
+    },
 };
